ababa34858112491c2146a4a8039af6927ae885e
sahar-mirtalebi <sahar.mirtalebi@gmail.com>
2017-06-30 14:26:26 +0430

    adding some file in gitignore and white first code in app.js

---
+++ app.js
@@ -0,0 +1,47 @@
+
+var fs = require('fs');
+var mm = require('musicmetadata');
+
+fs.readdir('./music/', function(err,files) {
+    if(err) throw err;
+    var jsonObj = {
+                    songs: []
+                };
+    files.forEach(function(file){
+        var jsonSong = { 
+                        title:"",
+                        artist:"",
+                        duration:"",
+                        pic:"",
+                        
+                    }
+        var parser = new mm(fs.createReadStream('./music/'+file));
+
+        parser.on('title', function(result) {
+            jsonSong.title = result;
+        });
+        parser.on('albumartist', function(result) {
+            jsonSong.artist = result;
+        });
+        parser.on('duration', function(result) {
+            jsonSong.duration = result;
+        });
+        parser.on('picture', function(result) {
+            jsonSong.pic = result;
+        });
+        
+        jsonObj.songs.push(jsonSong);
+
+        
+    });
+
+  	console.log(jsonObj);
+
+
+});
+
+
+
+
+
+
